f507a0a75f679c08f98ad754e4ac7e5273184174
Abbi Parchment <abbigail151@gmail.com>
2021-11-06 17:03:28 -0500

    Excercise 4 completed

--- world.php
+++ world.php
@@ -12,8 +12,6 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries");
-
 
 $country = strval($_GET['country']);
 $country = filter_var($country, FILTER_SANITIZE_STRING);
@@ -23,9 +21,26 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<body>
+  <table style = "width:70%">
+  <thead>
+    <tr>
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence</th>
+        <th>Head of State</th>
+      </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?php echo $row["name"]; ?></td>
+      <td><?php echo $row["continent"]; ?></td>
+      <td><?php echo $row["independence_year"]; ?></td>
+      <td><?php echo $row["head_of_state"]; ?></td>
+      </tr>
+      <?php endforeach; ?>
+    <tbody>
+</table>
+</body>
 
