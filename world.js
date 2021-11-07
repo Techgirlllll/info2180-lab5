43c098b015629dbee5c97c675d4a1e924868c750
Abbi Parchment <abbigail151@gmail.com>
2021-11-07 09:55:05 -0500

    Excercise 5 completed

--- world.js
+++ world.js
@@ -1,30 +1,38 @@
 window.onload = start;
+    var context = document.getElementById("cities");
 
-function start () {
-    var countryBtn = document.getElementById("lookup");
-    countryBtn.addEventListener("click", function(e) {
-        e.preventDefault();
-        console.log(countryBtn)
+    function start () {
+        button = document.querySelectorAll(".lookup");
+        button.forEach(button => {
+            button.addEventListener("click", function (e) {
+                e.preventDefault();
 
+            let result = document.getElementById("result");
+            let countries = document.getElementById("country").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g,'');
+            
+            if (button.id === "cities") {
+                context = "cities";
+            }
+            else {
+                context = "";
+            }
 
-    var result = document.getElementById("result");
-    var valInput = document.getElementById("country").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g,'');
-    
-    fetch(`http://localhost:8888/info2180-lab5/world.php?country=${valInput}`)
-    .then(response => {
-        if (response.ok) {
-            return response.text()
-        } else {
-            throw new Error ("There was a problem with the request");
-        }
-    })
-    .then(data => { 
-        result.innerHTML = "";
-        result.innerHTML = data; 
-    })
-    .catch(error => {
-        console.log(error);
-    });
+            fetch(`http://localhost:8888/info2180-lab5/world.php?country=${countries}&context=${context}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.text()
+                } else {
+                    throw new Error ("There was a problem with the request");
+                }
+            })
+            .then(data => { 
+                result.innerHTML = "";
+                result.innerHTML = data; 
+            })
+            .catch(error => {
+                console.log(error);
+            });
+});
 });
 };
 
--- world.php
+++ world.php
@@ -13,34 +13,65 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-$country = strval($_GET['country']);
+$country = strval($_GET["country"]);
 $country = filter_var($country, FILTER_SANITIZE_STRING);
+$context = strval($_GET["context"]);
+$context = filter_var($context, FILTER_SANITIZE_STRING);
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($context == "cities") {
+  $data= $conn->query("SELECT c.name, c.district, c.population FROM cities c join countries cs on c.country_code = cs.code WHERE cs.name LIKE '%$country%'");
+  $results = $data->fetchAll(PDO::FETCH_ASSOC);
+
+} else {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
 ?>
+<!-- <?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . 'is ruled by' .  $row['head_of_state']; ?></li>
+  <?php endforeach;?> -->
 
-<body>
+<?php if ($context == "cities"):?>
   <table style = "width:70%">
   <thead>
     <tr>
-        <th>Country Name</th>
-        <th>Continent</th>
-        <th>Independence</th>
-        <th>Head of State</th>
+        <th>City Name</th>
+        <th>District</th>
+        <th>Population</th>
       </tr>
   </thead>
   <tbody>
   <?php foreach ($results as $row): ?>
     <tr>
       <td><?php echo $row["name"]; ?></td>
-      <td><?php echo $row["continent"]; ?></td>
-      <td><?php echo $row["independence_year"]; ?></td>
-      <td><?php echo $row["head_of_state"]; ?></td>
+      <td><?php echo $row["district"]; ?></td>
+      <td><?php echo $row["population"]; ?></td>
       </tr>
       <?php endforeach; ?>
-    <tbody>
+    </tbody>
 </table>
-</body>
+
+<?php else: ?>
+  <table style = "width:70%">
+    <thead>
+      <tr>
+          <th>Country Name</th>
+          <th>Continent</th>
+          <th>Independence</th>
+          <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($results as $row): ?>
+        <tr>
+          <td><?php echo $row["name"]; ?></td>
+          <td><?php echo $row["continent"]; ?></td>
+          <td><?php echo $row["independence_year"]; ?></td>
+          <td><?php echo $row["head_of_state"]; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif; ?>
 
